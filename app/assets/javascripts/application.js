f291f2ca8cfcee954e5ee374a2dba0b1c1c8722c
Henry Morgan <hexmodr@gmail.com>
2016-07-12 14:02:53 +0100

    HI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,20 +20,20 @@ $(function(){
     var errors_found = false;
 
     $('[data-required]', $panel).find('input, select').each(function(){
-     if (this.value=='') {
-       $(this).closest('.field').addClass('field_with_errors')
-       errors_found = true;
-       console.log('errors', this)
-     } else {
-      console.log('asd')
-      $(this).closest('.field').removeClass('field_with_errors')
-    }
-  });
+
+      if (this.value=='') {
+        $(this).closest('.field').addClass('field_with_errors')
+        errors_found = true;
+        console.log('errors', this)
+      } else {
+       $(this).closest('.field').removeClass('field_with_errors')
+      }
+    });
     if (errors_found) {
      // alert('Fill in the fields')
    } else {
     $panel.removeClass('current_panel').next('.panel').addClass('current_panel');
-    $('.current_panel').find('input,select,textarea').first().trigger('focus')
+    $('.current_panel').trigger('current_panel').find('input,select,textarea').first().trigger('focus')
   }
 });
 
@@ -49,5 +49,36 @@ $(function(){
     return !$('.panel').last().hasClass('current_panel');
   }
 
+
+
+$('.summary_panel').on('current_panel', generateSummary);
+
+var summary_row_template = `<tr>
+  <th></th>
+  <td></td>
+</tr>`;
+
+function generateSummary() {
+  $summary_table = $('table.summary_table');
+  console.log($summary_table)
+  $summary_table.empty();
+  $('[data-summary]').each(function(){
+    var key = $(this).data('summary')
+    var value = $(this).find('input').val()
+    $row = $(summary_row_template);
+    $('th', $row).html(key)
+    $('td', $row).html(value)
+    $summary_table.append($row)
+
+  });
+}
+
+  $('[data-prev-panel]').on('click', function(){
+    $prev = $('.current_panel').prev('.panel')
+    if ($prev.length > 0) {
+      $('.current_panel').removeClass('current_panel')
+      $prev.addClass('current_panel')
+    }
+  })
 
 });
