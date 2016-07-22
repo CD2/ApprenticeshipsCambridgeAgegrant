2189a3a6849beb4f47d366a1e0b58ecc626fb14d
Shane <shane@cd2solutions.co.uk>
2016-07-22 12:59:36 +0100

    this

--- test/mailers/previews/admin_mailer_preview.rb
+++ test/mailers/previews/admin_mailer_preview.rb
@@ -2,8 +2,17 @@
 class AdminMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/admin_mailer/no_training_provider
-  def no_training_provider
-    AdminMailer.no_training_provider
+  def no_training_provider grant_detail
+    @grant_detail =
+    AdminMailer.no_training_provider()
+  end
+
+  def send_reminder
+
+  end
+
+  def no_review_15_weeks
+
   end
 
 end
