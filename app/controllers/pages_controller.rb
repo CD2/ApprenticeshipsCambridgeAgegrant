2ddc442093abebce6ff507483a2b84929592d1b6
Shane <shane@cd2solutions.co.uk>
2016-10-14 07:32:15 +0100

    this

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,7 +16,7 @@ class PagesController < ApplicationController
 
   def email_10_weeks_ago
     begin
-      puts GrantDetail.where("DATE(apprentice_start_date) <= :week AND reminder_email_sent = false", {:week => 10.weeks.ago}).count
+      puts GrantDetail.where("to_date(apprentice_start_date, 'DD/MM/YYYY') <= :week AND reminder_email_sent = false", {:week => 10.weeks.ago}).count
       GrantDetail.where("DATE(apprentice_start_date) <= :week AND reminder_email_sent = false", {:week => 10.weeks.ago}).each_with_index do |gd, i|
         if i == 0
           AdminMailer.send_reminder(gd, site_url).deliver_now
