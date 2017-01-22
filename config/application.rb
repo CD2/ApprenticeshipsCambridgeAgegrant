a649b2db3c160128377562d87cfaeb4cf735bec2
Shane Morgan <shane@cd2solutions.co.uk>
2017-01-22 18:35:01 +0000

    asd

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,6 @@ module ApprenticeshipsNorfolk
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w( ckeditor/* )
-    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
   end
 end
